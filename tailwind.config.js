5d76a52fbc6e43de7c562b7062ae28d2e6d57f67
Masud Afsar <masud.afsar@gmail.com>
2023-08-02 12:13:49 +0330

    Implement border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    borderRadius: {
+      sm: '8px',
+      md: '10px',
+      lg: '12px',
+    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
     },
