c7dc04a2fcab9595c1a12c41a136d68e621bb0da
Masud Afsar <masud.afsar@gmail.com>
2023-08-02 17:34:09 +0330

    Implement card component

--- src/components/cardHeader/component.tsx
+++ src/components/cardHeader/component.tsx
@@ -1,13 +1,9 @@
-import {PropsWithChildren, ReactNode} from "react";
+import {PropsWithChildren} from "react";
 import styles from './styles.module.scss';
 import {CardHeaderIcon} from "../cardHeaderIcon";
 import {CardHeaderTitle} from "../cardHeaderTitle";
 
 export interface CardHeaderPropsType {
-  icon?: ReactNode;
-  title?: string;
-  supportingText?: string;
-  actions?: ReactNode[];
 }
 
 export function CardHeader({children}: PropsWithChildren<CardHeaderPropsType>) {
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export * from './table';
 export * from './tableHeaderColumn';
 export * from './tableDataColumn';
 export * from './button';
+export * from './card';
+export * from "./cardHeaderIcon";
+export * from "./cardHeaderTitle";
