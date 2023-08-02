7b289b17c44a3a7608e694c3d9bb30a55b0d4b5b
Masud Afsar <masud.afsar@gmail.com>
2023-08-02 04:55:59 +0330

    Implement a story for button component

--- src/components/button/index.stories.tsx
+++ src/components/button/index.stories.tsx
@@ -9,6 +9,8 @@ const meta = {
   },
   argTypes: {
     title: {control: 'text'},
+    color: {},
+    variant: {},
   },
 } satisfies Meta<typeof Button>;
 
@@ -16,8 +18,50 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const PrimaryText: Story = {
   args: {
     title: "Primary",
+    color: 'primary',
+    variant:'text'
+  },
+};
+
+export const PrimaryOutline: Story = {
+  args: {
+    title: "Primary",
+    color: 'primary',
+    variant:'outline'
+  },
+};
+
+export const PrimaryFill: Story = {
+  args: {
+    title: "Primary",
+    color: 'primary',
+    variant:'fill'
+  },
+};
+
+export const SecondaryText: Story = {
+  args: {
+    title: "Secondary",
+    color: 'secondary',
+    variant:'text'
+  },
+};
+
+export const SecondaryOutline: Story = {
+  args: {
+    title: "Secondary",
+    color: 'secondary',
+    variant:'outline'
+  },
+};
+
+export const SecondaryFill: Story = {
+  args: {
+    title: "Secondary",
+    color: 'secondary',
+    variant:'fill'
   },
 };
