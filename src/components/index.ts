63595f3c0cd4ece3b649d9da0acb0b3e7dae066a
Masud Afsar <masud.afsar@gmail.com>
2023-08-02 19:53:23 +0330

    Implement table component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,13 @@
 export * from './table';
 export * from './tableHeaderColumn';
 export * from './tableDataColumn';
+
 export * from './button';
+
 export * from './card';
 export * from './cardHeader';
 export * from './cardBody';
 export * from "./cardHeaderIcon";
 export * from "./cardHeaderTitle";
+
 export * from "./filePicker";
