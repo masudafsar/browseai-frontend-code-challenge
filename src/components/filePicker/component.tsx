dda3f21222c4c91bba47fc475579bad0714b4b27
Masud Afsar <masud.afsar@gmail.com>
2023-08-03 21:18:38 +0330

    Implement file picker as forward ref

--- src/components/filePicker/component.tsx
+++ src/components/filePicker/component.tsx
@@ -1,33 +1,30 @@
-import {ChangeEventHandler} from "react";
+import {forwardRef, InputHTMLAttributes} from "react";
 import {UploadCloudIcon} from "../../icons/uploadCloudIcon";
 import styles from './styles.module.scss';
 
-export interface FilePickerPropsType {
-  onPickFile: ChangeEventHandler<HTMLInputElement>;
+export interface FilePickerPropsType extends InputHTMLAttributes<HTMLInputElement> {
 }
 
-export function FilePicker({onPickFile}: FilePickerPropsType) {
-  return (
-    <div className={styles.FilePicker_Root}>
-      <label className={styles.FilePicker_Picker}>
+export const FilePicker = forwardRef<HTMLInputElement, FilePickerPropsType>((props, ref) => (
+  <div className={styles.FilePicker_Root}>
+    <label className={styles.FilePicker_Picker}>
 
-        <div className={styles.FilePicker_UploadIcon}>
-          <UploadCloudIcon/>
-        </div>
+      <div className={styles.FilePicker_UploadIcon}>
+        <UploadCloudIcon/>
+      </div>
 
-        <p className={styles.FilePicker_HelpNote}>
-          <span>Click to upload</span>
-          {' '}
-          or drag and drop
-        </p>
+      <p className={styles.FilePicker_HelpNote}>
+        <span>Click to upload</span>
+        {' '}
+        or drag and drop
+      </p>
 
-        <input
-          type="file"
-          onChange={onPickFile}
-          className="hidden"
-          accept=".csv"
-        />
-      </label>
-    </div>
-  )
-}
+      <input
+        {...props}
+        ref={ref}
+        type="file"
+        className="hidden"
+      />
+    </label>
+  </div>
+));
