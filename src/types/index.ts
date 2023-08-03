2fb0405a2dd08e56716a0c9d5c90b8102ab727c8
Masud Afsar <masud.afsar@gmail.com>
2023-08-03 22:16:55 +0330

    Implement search case type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from './colorThemeType';
 export * from './currentPageType';
 export * from './reviewTableDataType';
 export * from './resultTableDataType';
+export * from './searchCaseType';
