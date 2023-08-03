4da6e67f29e18a1b580133a7e62c890181622520
Masud Afsar <masud.afsar@gmail.com>
2023-08-04 01:35:06 +0330

    Import left arrow icon

---
+++ src/icons/arrowLeftIcon.tsx
@@ -0,0 +1,12 @@
+export function ArrowLeftIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+      <path
+        d="M16.6668 10H3.3335M3.3335 10L8.3335 15M3.3335 10L8.3335 5"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
