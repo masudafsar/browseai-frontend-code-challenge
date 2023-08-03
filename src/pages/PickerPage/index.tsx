663260ed62f8ac31df398c95b1202b28004fb37b
Masud Afsar <masud.afsar@gmail.com>
2023-08-03 22:27:25 +0330

    Implement picker page

--- src/pages/PickerPage/index.tsx
+++ src/pages/PickerPage/index.tsx
@@ -1,12 +1,48 @@
-import {ChangeEventHandler} from "react";
+import {ChangeEvent, useCallback, useContext, useState} from "react";
 import {GitHubIcon} from "../../icons/githubIcon";
 import {Card, FilePicker} from "../../components";
+import {AppContext} from "../../contexts/appContext";
+import {SearchCaseType} from "../../types";
+import {useNavigate} from "react-router-dom";
 
 export interface PickerPagePropsTypes {
-  setSelectedFile: ChangeEventHandler<HTMLInputElement>;
 }
 
-export function PickerPage({setSelectedFile}: PickerPagePropsTypes) {
+export function PickerPage({}: PickerPagePropsTypes) {
+  const {setSearchCases} = useContext(AppContext);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const pickFileHandler = useCallback(async function (event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files || event.target.files.length === 0) return;
+    const reader = new FileReader();
+    reader.onloadstart = async function () {
+      setIsLoading(true);
+    }
+    reader.onload = async function (event) {
+      const rawText = event.target?.result;
+      if (typeof rawText !== "string") return;
+      const cases: SearchCaseType[] = [];
+      rawText.split('\n').forEach((line) => {
+        const [searchKeywords, username, context] = line.split(',');
+        const searchCase: SearchCaseType = {
+          data: {
+            searchKeywords,
+            username,
+            context,
+          },
+          results: [],
+          status: "idle",
+        };
+        cases.push(searchCase);
+      });
+      setSearchCases(cases);
+    }
+    reader.readAsText(event.target.files?.[0]);
+    setIsLoading(false);
+    navigate('/review');
+  }, [setSearchCases]);
+
   return (
     <Card>
       <Card.Header>
@@ -17,7 +53,13 @@ export function PickerPage({setSelectedFile}: PickerPagePropsTypes) {
         />
       </Card.Header>
       <Card.Body>
-        <FilePicker onPickFile={setSelectedFile}/>
+        <div className="p-6 pt-4">
+          <FilePicker
+            disabled={isLoading}
+            onChange={pickFileHandler}
+            accept=".csv"
+          />
+        </div>
       </Card.Body>
     </Card>
   );
