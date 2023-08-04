5a02cb3597005e4350245bd0a29a91ed3c16b471
Masud Afsar <masud.afsar@gmail.com>
2023-08-05 02:43:01 +0330

    Implement CSV reader to compatible with different column orders

--- src/utils/searchCase.ts
+++ src/utils/searchCase.ts
@@ -1,24 +1,31 @@
 import {type SearchCaseType} from "../types";
 
 export function rawTextToSearchCases(raw: string): Array<SearchCaseType> {
-  const lines = raw.split('\n').map(line => line.replace('\r', ''));
+  const [header, ...lines] = raw.split('\n').map(line => line.replace('\r', ''));
   const cases: SearchCaseType[] = [];
   const uniqueLines = new Set<string>();
+
+  let searchKeywordsIndex = header.split(',').findIndex(value => value.replace(' ', '').toLowerCase() === 'searchKeywords'.toLowerCase());
+  searchKeywordsIndex = searchKeywordsIndex !== -1 ? searchKeywordsIndex : 0
+  let usernameIndex = header.split(',').findIndex(value => value.replace(' ', '').toLowerCase() === 'username'.toLowerCase());
+  usernameIndex = usernameIndex !== -1 ? usernameIndex : 1
+  let contextIndex = header.split(',').findIndex(value => value.replace(' ', '').toLowerCase() === 'context'.toLowerCase());
+  contextIndex = contextIndex !== -1 ? contextIndex : 2
 
   lines.forEach((line) => {
     const isDuplicate = uniqueLines.has(line);
     if (!isDuplicate) uniqueLines.add(line);
 
-    const [searchKeywords, username, context] = line.split(',');
+    const cells = line.split(',');
 
     const searchCase: SearchCaseType = {
       data: {
-        searchKeywords,
-        username,
-        context,
+        searchKeywords: cells[searchKeywordsIndex],
+        username: cells[usernameIndex],
+        context: cells[contextIndex],
       },
       count: 0,
-      status: searchKeywords && !isDuplicate ? "idle" : "invalid",
+      status: cells[searchKeywordsIndex] && !isDuplicate ? "idle" : "invalid",
     };
     cases.push(searchCase);
   });
